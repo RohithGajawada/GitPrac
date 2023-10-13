c68ef6640be1bb0605a1b3a5cfcaea3eef914e9b
RohithGajawada <21b81a3336@cvr.ac.in>
2023-10-13 20:44:53 +0530

    done

---
+++ anagram.js
@@ -0,0 +1,19 @@
+function sort(str1){
+    var arr = str1.split('');
+    arr = arr.sort();
+    arr = arr.join();
+
+    return arr;
+}
+
+function anagramCheck(str1, str2){
+    if(sort(str1) == sort(str2)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+var ans = anagramCheck('abcc', 'ccba');
+console.log(ans);
